d38b843b37a9e4c27c7d6db6b4fd807d182827ba
junshen2110 <143473514+junshen2110@users.noreply.github.com>
2023-08-29 11:42:14 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "database-1.ctktvos05fp8.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "tehjunshen-employee"
+customregion = "us-east-1"
+
